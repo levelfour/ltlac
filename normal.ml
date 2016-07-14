f19c3847cb8c0a0686a8697cde9ad041f4df0106
levelfour <levelfourslv@gmail.com>
2016-07-14 12:29:10 +0900

    eliminate double negation

--- normal.ml
+++ normal.ml
@@ -7,13 +7,26 @@ open Syntax
  * 
  *)
 
-let rec f = function
-  | Not(e) -> Not(f e)
-  | Or(e1, e2) -> Or(f e1, f e2)
-  | And(e1, e2) -> Not(Or(Not(f e1), Not(f e2)))
-  | Next(e) -> Next(f e)
-  | Future(e) -> Until(Bool(true), f e)
-  | Globally(e) -> Not(Until(Bool(true), Not(f e)))
-  | Until(e1, e2) -> Until(f e1, f e2)
-  | Release(e1, e2) -> Not(Until(Not(f e1), Not(f e2)))
+let rec normalize = function
+  | Not(e) -> Not(normalize e)
+  | Or(e1, e2) -> Or(normalize e1, normalize e2)
+  | And(e1, e2) -> Not(Or(Not(normalize e1), Not(normalize e2)))
+  | Next(e) -> Next(normalize e)
+  | Future(e) -> Until(Bool(true), normalize e)
+  | Globally(e) -> Not(Until(Bool(true), Not(normalize e)))
+  | Until(e1, e2) -> Until(normalize e1, normalize e2)
+  | Release(e1, e2) -> Not(Until(Not(normalize e1), Not(normalize e2)))
   | e -> e
+
+let rec elim_dneg = function
+  | Not(Bool(false)) -> Bool(true)
+  | Bool(false) -> Not(Bool(true))
+  | Not(Not(e)) -> elim_dneg e
+  | Not(e) -> Not(elim_dneg e)
+  | Or(e1, e2) -> Or(elim_dneg e1, elim_dneg e2)
+  | Next(e) -> Next(elim_dneg e)
+  | Until(e1, e2) -> Until(elim_dneg e1, elim_dneg e2)
+  | e -> e
+
+let rec f e = 
+  elim_dneg (normalize e)
